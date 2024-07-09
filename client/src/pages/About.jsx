2b98f42d42f13aa453b84ab00c0df5e4a24029e2
Cotten-Lumb03 <cottenlumb@ufl.edu>
2024-07-09 17:42:35 -0400

    Added some information to the About Us and Contact Us pages.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,12 +5,25 @@ import Footer from "components/Footer";
 
 function About() {
   return (
-    <h1>
-      <Banner />
-      <Navbar />
-      About Albert's List
-      <Footer />
-    </h1>
+      <div>
+          <div>
+              <Banner />
+              <Navbar />
+              <body className="center">
+              <div>About Albert's List</div>
+              <div>Popular services offered by fellow partners and given to users who wish for services</div>
+              <div>
+                  Helping to grow other services since 2024.
+                  <p>We began off as a small company from Florida, and managed to grow our services across the country</p>
+              </div>
+              <div>You can find our number within our Contact Page. If you have any questions, feel free to contact one of our providers</div>
+              <div>
+                  <p> </p>
+              </div>
+              </body>
+          </div>
+          <Footer />
+      </div>
   );
 }
 
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -5,12 +5,25 @@ import Footer from "components/Footer";
 
 function Contact() {
   return (
-    <div>
-      <Banner />
-      <Navbar />
-      Contact Albert's List
-      <Footer />
-    </div>
+      <div>
+          <div>
+              <Banner />
+              <Navbar />
+              <body className="center">
+              <div>Contact Us</div>
+              <div>Feel Free to Contact Us at our local number</div>
+              <div>
+                  123-456-7890
+                  <p>Our office ours are between 9am-5pm on the weekdays</p>
+              </div>
+              <div>On the weekends, we are available to take calls between 9am-4pm. </div>
+              <div>
+                  <p> </p>
+              </div>
+              </body>
+          </div>
+          <Footer />
+      </div>
   );
 }
 
