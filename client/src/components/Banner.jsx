35dcc724ce8027cab9aa873ea5a771522e5625ea
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-07-10 19:36:15 -0400

    Merging

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import logo from "../assets/images/logo.png";
 import User from "./User";
 import { Link } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
 
 function Banner() {
   return (
-    <div className="banner flex flex-row gap-3">
+    <FlexBetween className="banner">
       <Link to="/">
-        <img height={5} src={logo} alt="Logo" />
+        <img
+          src={logo}
+          alt="Logo"
+          style={{
+            maxWidth: "60%",
+            height: "auto",
+            display: "block",
+          }}
+        />
       </Link>
 
       <User />
-    </div>
+    </FlexBetween>
   );
 }
 
