778e82bdfe449eac7b8e702e725bddb4fb75493a
devdrum <bdrumgool@gmail.com>
2024-07-10 19:27:24 -0400

    backend work

---
+++ Server/server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+
+
+// For parsing application/json
+app.use(express.json());
+ 
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: '55816',
+  user: 'dev',
+  password: 'dev',
+  database: 'albertslist'
+})
+
+// Test API call
+app.get("/api", (req, res) => {
+    res.json({ "users": ["user1", "user2", "user3"] })
+})
+
+// Test DB Call
+app.get("/api/users", (req, res) => {
+    connection.connect()
+
+    connection.query('SELECT * from Users', (err, result) => {
+  if (err) throw err  
+
+
+res.json(result);
+    
+})
+connection.end();
+})
+
+// Test Insert User
+app.post("/api/post/insertuser", (req, res) => {
+
+    var username = req.body.username
+    var password = req.body.password
+    var firstname = req.body.firstname
+    var lastname = req.body.lastname
+    var email = req.body.email
+    var isPartner = req.body.isPartner
+    var zip = req.body.zip
+    
+    connection.connect();
+    var sql = "INSERT INTO Users (username, password, firstname,lastname,email,isPartner,zip) VALUES('"+username+"','"+password+"','"+firstname+"','"+lastname+"','"+email+"','"+isPartner+"','"+zip+"')"
+    connection.query(sql, (err, result) => {
+  if (err) throw err
+  res.send({
+    message: 'New user:'+username+' was added to the database',
+  });
+    
+})
+connection.end()
+})
+
+app.listen(5002, () => { console.log("Server started on port 5002") })
