b779ef48d594553e42a51182587c14d944a5ed80
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-07-11 18:03:11 -0400

    Server Work

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
+const PORT = 5002;
 
 // For parsing application/json
 app.use(express.json());
@@ -10,6 +11,39 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const mysql = require("mysql");
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: "{https://booking-api/}",
+  issuerBaseURL: `dev-gh4kxlfpg0ce2ig2.us.auth0.com`,
+});
+
+// This route doesn't need authentication
+app.get("/api/public", function (req, res) {
+  res.json({
+    message:
+      "Hello from a public endpoint! You don't need to be authenticated to see this.",
+  });
+});
+
+// This route needs authentication
+app.get("/api/private", checkJwt, function (req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated to see this.",
+  });
+});
+
+const checkScopes = requiredScopes("read:messages");
+
+app.get("/api/private-scoped", checkJwt, checkScopes, function (req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
+  });
+});
+
 const connection = mysql.createConnection({
   host: "localhost",
   port: "55816",
