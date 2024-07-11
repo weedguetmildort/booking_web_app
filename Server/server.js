7e5c07e2a976b9ca47e24f2448f41b363ed0c88f
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-07-11 16:50:54 -0400

    Server

--- Server/server.js
+++ Server/server.js
@@ -1,62 +1,80 @@
-const express = require('express')
-const app = express()
+const express = require("express");
 
+const app = express();
+const PORT = 3000;
 
 // For parsing application/json
 app.use(express.json());
- 
+
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const mysql = require('mysql')
+const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: '55816',
-  user: 'dev',
-  password: 'dev',
-  database: 'albertslist'
-})
+  host: "localhost",
+  port: "55816",
+  user: "dev",
+  password: "dev",
+  database: "albertslist",
+});
 
 // Test API call
 app.get("/api", (req, res) => {
-    res.json({ "users": ["user1", "user2", "user3"] })
-})
+  res.json({ users: ["user1", "user2", "user3"] });
+});
 
 // Test DB Call
 app.get("/api/users", (req, res) => {
-    connection.connect()
+  connection.connect();
 
-    connection.query('SELECT * from Users', (err, result) => {
-  if (err) throw err  
+  connection.query("SELECT * from Users", (err, result) => {
+    if (err) throw err;
 
-
-res.json(result);
-    
-})
-connection.end();
-})
+    res.json(result);
+  });
+  connection.end();
+});
 
 // Test Insert User
 app.post("/api/post/insertuser", (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var email = req.body.email;
+  var isPartner = req.body.isPartner;
+  var zip = req.body.zip;
 
-    var username = req.body.username
-    var password = req.body.password
-    var firstname = req.body.firstname
-    var lastname = req.body.lastname
-    var email = req.body.email
-    var isPartner = req.body.isPartner
-    var zip = req.body.zip
-    
-    connection.connect();
-    var sql = "INSERT INTO Users (username, password, firstname,lastname,email,isPartner,zip) VALUES('"+username+"','"+password+"','"+firstname+"','"+lastname+"','"+email+"','"+isPartner+"','"+zip+"')"
-    connection.query(sql, (err, result) => {
-  if (err) throw err
-  res.send({
-    message: 'New user:'+username+' was added to the database',
+  connection.connect();
+  var sql =
+    "INSERT INTO Users (username, password, firstname,lastname,email,isPartner,zip) VALUES('" +
+    username +
+    "','" +
+    password +
+    "','" +
+    firstname +
+    "','" +
+    lastname +
+    "','" +
+    email +
+    "','" +
+    isPartner +
+    "','" +
+    zip +
+    "')";
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send({
+      message: "New user:" + username + " was added to the database",
+    });
   });
-    
-})
-connection.end()
-})
+  connection.end();
+});
 
-app.listen(5002, () => { console.log("Server started on port 5002") })
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
