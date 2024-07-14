8f2f97bb9160907dc7084a0b3bde847f7b9cce30
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-07-14 00:13:09 -0400

    Fixes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql");
+const https = require("node:https");
+
+// AUTH0 REQUIRED IMPORTS
+const session = require("express-session");
+const routes = require("./routes");
 
 const app = express();
-const port = 5000;
+const PORT = 5002;
+// const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+// For parsing application/json
 app.use(express.json());
 
-app.listen(port, function () {
-    console.log('Listening on http://localhost:' + port);
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// AUTH0 - Transitioning to modularity structure
+// LOGIN - Setup session middleware
+app.use(
+  session({
+    secret: "app_session_secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }, // Set to true in production
+  })
+);
+
+// MAIN LOGIN LOGIC -- NEEDS TO BE TESTED
+app.use(routes);
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
 });
