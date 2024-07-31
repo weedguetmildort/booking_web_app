08401bcad06fc58fe17b6bc7071abdc2316b0b62
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-07-31 10:37:07 -0400

    Google API

---
+++ server/routes/address.js
@@ -0,0 +1,57 @@
+require("dotenv").config({ path: "../.env" });
+
+const express = require("express");
+
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+
+const router = express.Router();
+
+const domain = process.env.GOOGLE_API_ADDRESS_DOMAIN;
+const api = process.env.GOOGLE_API_ADDRESS_API;
+const apiKey = process.env.GOOGLE_API_ADDRESS_KEY;
+
+// Check address
+router.post("/api/checkAddress", async (req, res) => {
+  const { street, city, state, zip } = req.body;
+
+  const address = {
+    regionCode: "US",
+    addressLines: [street, city, state, zip],
+  };
+
+  try {
+    const response = await axios.post(
+      `https://${domain}/v1:${api}?key=${apiKey}`,
+      { address }
+    );
+
+    res.json({
+      data: {
+        street:
+          response.data.result.uspsData.standardizedAddress.firstAddressLine,
+        city: response.data.result.uspsData.standardizedAddress.city,
+        state: response.data.result.uspsData.standardizedAddress.state,
+        zip: response.data.result.uspsData.standardizedAddress.zipCode,
+      },
+    });
+  } catch (error) {
+    if (error.response) {
+      res.status(error.response.status).json({
+        error: error.response.data,
+      });
+    } else if (error.request) {
+      res.status(500).json({
+        message: "No response received from the server",
+        error: error.message,
+      });
+    } else {
+      res.status(500).json({
+        message: "Error in setting up the request",
+        error: error.message,
+      });
+    }
+  }
+});
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoutes = require("./auth");
 const dbRoutes = require("./db");
+const addressRoutes = require("./address");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/db", dbRoutes);
+router.use("/address", addressRoutes);
 
 module.exports = router;
